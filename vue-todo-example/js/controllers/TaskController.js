9b2e44d2a15ec46e9e3522ec6b8972525f5994d4
Lino De La Cruz <dave@Linos-MacBook-Air.local>
2026-08-05 13:24:11 +0800

    Implement vue-todo-example TaskController

--- vue-todo-example/js/controllers/TaskController.js
+++ vue-todo-example/js/controllers/TaskController.js
@@ -1,1 +1,50 @@
-var TaskController = {};
+var TaskController = {
+  getTasks: function () {
+    return fetch('./Backend/api/get_tasks.php').then(function (response) {
+      return response.json();
+    });
+  },
+
+  createTask: function (task) {
+    var formData = new FormData();
+    formData.append('title', task.title);
+    formData.append('notes', task.notes);
+    formData.append('category', task.category);
+    formData.append('status', task.status);
+
+    return fetch('./Backend/api/create_task.php', {
+      method: 'POST',
+      body: formData,
+    }).then(function (response) {
+      return response.json();
+    });
+  },
+
+  updateTask: function (task) {
+    var formData = new FormData();
+    formData.append('id', task.id);
+    formData.append('title', task.title);
+    formData.append('notes', task.notes);
+    formData.append('category', task.category);
+    formData.append('status', task.status);
+
+    return fetch('./Backend/api/update_task.php', {
+      method: 'POST',
+      body: formData,
+    }).then(function (response) {
+      return response.json();
+    });
+  },
+
+  deleteTask: function (id) {
+    var formData = new FormData();
+    formData.append('id', id);
+
+    return fetch('./Backend/api/delete_task.php', {
+      method: 'POST',
+      body: formData,
+    }).then(function (response) {
+      return response.json();
+    });
+  },
+};
