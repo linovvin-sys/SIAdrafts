e4ed220e72d8f3d1ce5fe607e9412227cefabb77
Lino De La Cruz <dave@Linos-MacBook-Air.local>
2026-08-05 13:23:19 +0800

    Implement vue-todo-example LoginView and RegisterView

--- vue-todo-example/js/views/LoginView.js
+++ vue-todo-example/js/views/LoginView.js
@@ -1,3 +1,34 @@
+var { ref } = Vue;
+
 var LoginView = {
-  template: `<div class="page"><h1>Log In (stub)</h1></div>`,
+  setup() {
+    var email = ref('');
+    var password = ref('');
+    var errorMsg = ref('');
+
+    function submit() {
+      errorMsg.value = '';
+      AuthController.login(email.value, password.value).then(function (data) {
+        if (data.error) {
+          errorMsg.value = data.error;
+        } else {
+          window.location.hash = '#/';
+        }
+      });
+    }
+
+    return { email, password, errorMsg, submit };
+  },
+  template: `
+    <div class="page">
+      <h1>Log In</h1>
+      <form @submit.prevent="submit">
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Log In</button>
+      </form>
+      <p class="error" v-if="errorMsg">{{ errorMsg }}</p>
+      <p><router-link to="/register">Need an account? Register</router-link></p>
+    </div>
+  `,
 };
--- vue-todo-example/js/views/RegisterView.js
+++ vue-todo-example/js/views/RegisterView.js
@@ -1,3 +1,44 @@
+var { ref } = Vue;
+
 var RegisterView = {
-  template: `<div class="page"><h1>Register (stub)</h1></div>`,
+  setup() {
+    var name = ref('');
+    var email = ref('');
+    var password = ref('');
+    var confirmPassword = ref('');
+    var errorMsg = ref('');
+
+    function submit() {
+      errorMsg.value = '';
+
+      if (password.value !== confirmPassword.value) {
+        errorMsg.value = 'Passwords do not match.';
+        return;
+      }
+
+      AuthController.register(name.value, email.value, password.value, confirmPassword.value).then(function (data) {
+        if (data.error) {
+          errorMsg.value = data.error;
+        } else {
+          window.location.hash = '#/';
+        }
+      });
+    }
+
+    return { name, email, password, confirmPassword, errorMsg, submit };
+  },
+  template: `
+    <div class="page">
+      <h1>Register</h1>
+      <form @submit.prevent="submit">
+        <input v-model="name" placeholder="Name" required />
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <input v-model="confirmPassword" type="password" placeholder="Confirm password" required />
+        <button type="submit">Register</button>
+      </form>
+      <p class="error" v-if="errorMsg">{{ errorMsg }}</p>
+      <p><router-link to="/login">Already have an account? Log in</router-link></p>
+    </div>
+  `,
 };
